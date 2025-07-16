8c33694c5a026d275258bddf53e72d978e0b7b82
Luis Felipe <luisfelipe24082003@gmail.com>
2025-07-16 11:43:52 -0300

    Adjusts: Hover effects

--- src/modules/agendamento/components/filter/index.tsx
+++ src/modules/agendamento/components/filter/index.tsx
@@ -20,20 +20,29 @@ const Filter: React.FC<IFilterProps> = ({ setSearch, setOrder, setPage, order, h
 
   return (
     <div className="h-10 flex flex-row items-center">
-      <input 
-        type="text" 
-        placeholder="Buscar agendamento" 
+      <input
+        type="text"
+        placeholder="Buscar agendamento"
         className="h-full w-[300px] px-4 rounded-l-md border border-gray-200"
         onChange={(e) => setSearch(e.target.value)}
       />
-      <button className="h-full px-4 cursor-pointer bg-blue-800 border-none text-white rounded-r-md" onClick={handleSearch}>
+      <button
+        className="h-full px-4 cursor-pointer bg-blue-700 hover:bg-blue-900 transition-colors text-white rounded-r-md"
+        onClick={handleSearch}
+      >
         <p className="text-sm font-semibold">Buscar</p>
       </button>
-      <button className="border-none p-0 text-red-400 cursor-pointer flex flex-row items-center gap-2 ml-4" onClick={handleClear}>
+      <button
+        className="border-none p-0 text-red-400 hover:text-red-500 transition-colors cursor-pointer flex flex-row items-center gap-2 ml-4"
+        onClick={handleClear}
+      >
         <p className="text-sm"> Limpar </p>
       </button>
-      <button data-tooltip-id="order-tooltip" className="border-none p-0 text-blue-400 cursor-pointer flex flex-row items-center gap-2 ml-4">
-        <FunnelIcon className="size-4"/>
+      <button
+        data-tooltip-id="order-tooltip"
+        className="border-none p-0 text-blue-400 hover:text-blue-500 transition-colors cursor-pointer flex flex-row items-center gap-2 ml-4"
+      >
+        <FunnelIcon className="size-4" />
         <p className="text-sm"> Ordenar </p>
       </button>
       <Tooltip
