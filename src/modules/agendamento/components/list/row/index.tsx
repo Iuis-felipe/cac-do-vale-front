c03311620811d93a8383209d885b88153e2f6562
Luis Felipe <luisfelipe24082003@gmail.com>
2025-07-24 09:14:37 -0300

    Fix: Agendamentos com nomes corrigidos

--- src/modules/agendamento/components/list/row/index.tsx
+++ src/modules/agendamento/components/list/row/index.tsx
@@ -8,36 +8,36 @@ interface ITableRowProps {
 }
 
 const TableRow: React.FC<ITableRowProps> = ({ data, handleActions }) => {
-  const { nome_social, telefone, cpf, categoria, origem, dia, status } = data;
-  
+  const { nome_civil, nome_social, telefone, cpf, categoria, origem, dia, status } = data;
+
   const statusColor = (status: string) => {
-    if(status === "pendente") {
-      return "text-orange-500"
+    if (status === "pendente") {
+      return "text-orange-500";
     }
 
-    if(status === "cancelado") {
-      return "text-red-500"
+    if (status === "cancelado") {
+      return "text-red-500";
     }
 
-    return "text-green-500"
-  }
+    return "text-green-500";
+  };
 
   return (
     <div className="grid grid-cols-9 border border-gray-200">
-      <CellItem text={nome_social} colSpan={2}/>
+      <CellItem text={nome_social || nome_civil} colSpan={2} />
       <CellItem text={telefone} />
       <CellItem text={cpf} />
       <CellItem text={categoria} />
       <CellItem text={origem} />
       <CellItem text={format(new Date(dia), "dd/MM/yyyy HH:mm")} />
-      <CellItem text={status} textColor={statusColor(status)}/>
+      <CellItem text={status} textColor={statusColor(status)} />
       <div className="flex flex-row items-center justify-center gap-3 p-2">
         <button className="p-0 cursor-pointer" onClick={() => handleActions(data.id)}>
-          <Cog8ToothIcon className="size-6 text-slate-600"/>
+          <Cog8ToothIcon className="size-6 text-slate-600" />
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default TableRow;
