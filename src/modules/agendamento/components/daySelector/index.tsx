7cd107b99519591902bc8ae504ad05619f904cbb
Luis Felipe <luisfelipe24082003@gmail.com>
2025-08-11 17:26:06 -0300

    Revert "Fix: Remover os horários já cadastrados."
    This reverts commit 2d8035a3afc4505e2bdb868ad682e2de7276584e.

--- src/modules/agendamento/components/daySelector/index.tsx
+++ src/modules/agendamento/components/daySelector/index.tsx
@@ -15,28 +15,27 @@ interface IDaySelector {
 }
 
 const DaySelector: React.FC<IDaySelector> = ({ loading, start, end, interval, intervalThreshold, unavailableHours, selectedTime, setSelectedTime }) => {
-  const [hours, setHours] = useState<{ time: string; available: boolean }[]>([])
+  const [hours, setHours] = useState<{ time: string; available: boolean }[]>([])  
 
   useEffect(() => {
-    const allHours = generateTimeSlots(start, end, unavailableHours, interval, intervalThreshold);
-    const availableHours = allHours.filter(hour => hour.available);
-
-    setHours(availableHours);
+    const hours = generateTimeSlots(start, end, unavailableHours, interval, intervalThreshold);
+    
+    setHours(hours);
   }, [start, end, interval, intervalThreshold, unavailableHours])
-
-  if (loading) return <Loader2 className="w-4 h-4 animate-spin" />
+  
+  if(loading) return <Loader2 className="w-4 h-4 animate-spin" />
 
   return (
     <div className="flex flex-row flex-wrap items-center gap-2 mt-4">
       {hours.map((it: { time: string; available: boolean }) => {
         return (
-          <div
-            onClick={() => setSelectedTime(it.time)}
-            key={it.time}
+          <div 
+            onClick={() => it.available && setSelectedTime(it.time)}
+            key={it.time} 
             className={`
               w-16 h-10 
               ${selectedTime === it.time ? 'border-1 border-sky-600' : ''}
-              bg-gray-200 cursor-pointer hover:bg-gray-300
+              ${it.available ? 'bg-gray-200 cursor-pointer hover:bg-gray-300' : 'bg-red-200'} 
               rounded-md flex items-center justify-center
             `}
           >
