a20190abd00a4de3062a2cdd6927b6477a0cf4ac
Gabriel Ryudi <azuma07ry@gmail.com>
2025-08-18 20:00:01 -0300

    Ajustes gerais

--- src/modules/horarios/hooks/useUpdateHorario.ts
+++ src/modules/horarios/hooks/useUpdateHorario.ts
@@ -3,7 +3,7 @@ import { updateSchedule } from "../services";
 
 interface IUpdateHorario {
   id: string;
-  body: { horarioStart?: string, horarioEnd?: string, intervalo?: string, intervaloThreshold?: string, isClosed?: boolean }
+  body: { horarioStart?: string, horarioEnd?: string, intervalo?: string, intervaloThreshold?: string, isHoliday?: boolean }
 }
 
 const useUpdateHorario = () => {
